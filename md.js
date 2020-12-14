0087d20261b306a9fb66783931972af62f5a20d4
mohammad kaif <mohammadkaif051197@gmail.com>
2020-12-14 13:02:18 +0530

    yes me

---
+++ md.js
@@ -0,0 +1,21 @@
+console.log("this is tutorial for funtion");
+function greet(name ,greetText){
+    console.log(greetText +" "+ name)
+    console.log(name +"  is a bad boy");
+}
+let name ="monu";
+let name1 ="monu1";
+let name3 ="monu3";
+let name2 ="monu2";
+let name4 ="monu4";
+let greetText ="GOOD MORNING";
+greet(name1,greetText);
+greet(name2,greetText);
+greet(name3,greetText);
+greet(name4,greetText);
+greet(name,greetText);
+// console.log(name +" is a bad boy");
+// console.log(name1 +" is a bad boy");
+// console.log(name2 +" is a bad boy");
+// console.log(name3 +" is a bad boy");
+// console.log(name4 +" is a bad boy");
