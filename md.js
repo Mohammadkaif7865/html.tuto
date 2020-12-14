75f6ec875922e6ecf35888fa6ef1548c7e9fdf58
mohammad kaif <mohammadkaif051197@gmail.com>
2020-12-14 18:40:40 +0530

    yesme

--- md.js
+++ md.js
@@ -1,7 +1,14 @@
 console.log("this is tutorial for funtion");
-function greet(name ,greetText){
+function greet(name ,greetText=" this is default arguement"){
+    let name1 ="manu";
     console.log(greetText +" "+ name)
     console.log(name +"  is a bad boy");
+}
+function sum(a,b,c)
+{ 
+    let d = a+b+c;
+    return d;
+    //console.log("this is a terminating point");
 }
 let name ="monu";
 let name1 ="monu1";
@@ -14,6 +21,10 @@ greet(name2,greetText);
 greet(name3,greetText);
 greet(name4,greetText);
 greet(name,greetText);
+// let returnval = greet(name3);
+let returnval = sum(1,2,3);
+console.log( "sum:" + returnval);
+
 // console.log(name +" is a bad boy");
 // console.log(name1 +" is a bad boy");
 // console.log(name2 +" is a bad boy");
